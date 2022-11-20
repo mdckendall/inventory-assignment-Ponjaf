488bf997a873d23814697b43bbae1a176d7b949a
UhhEddie <108373480+Ponjaf@users.noreply.github.com>
2022-11-20 23:53:36 +0000

    case 2 sucessfully completed I think? there isn't any errors so far

--- Main.java
+++ Main.java
@@ -21,25 +21,45 @@ class Main {
 
     userInput = sc.nextInt();
 
-    while(userInput != 5){
+    while(userInput != 5){  //This will determine the outcome based off what input was given
       switch(userInput){
         case 1:
 
           Inventory input = new Inventory();
 
           sc.nextLine();
-          System.out.println("Enter the name:");
+          System.out.println
+            ("Enter the name:");
           input.name = sc.nextLine();
-          System.out.println("Enter the serial number:");
+          System.out.println
+            ("Enter the serial number:");
           input.serialNumber = sc.nextLine();
-          System.out.println("Enter the value in dollars (whole number):");
+          System.out.println
+            ("Enter the value in dollars (whole number):");
           input.value = sc.nextInt();
 
           Items.add(input);
           break;
+
+        case 2:
+
+          sc.nextLine();
+          System.out.println
+            ("Enter the serial number of the item to delete:");
+          String replacementString = sc.nextLine();
+
+          Items.removeIf(n -> n.serialNumber.equals(replacementString));
+          break;
+
+
         
       }
-      
+            System.out.println("Press 1 to add an item.");
+      System.out.println("Press 2 to delete an item.");
+      System.out.println("Press 3 to update an item.");
+      System.out.println("Press 4 to show all the items.");
+      System.out.println("Press 5 to quit the program.");
+      userInput = sc.nextInt();
     }
 
 	}
